88ae329141b28e6dbc5249b2c68c09ebf1a177df
Cian <cian.omondi@student.moringaschool.com>
2021-10-06 09:51:55 +0300

    authentication

--- backend/student/admin.py
+++ backend/student/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import StudentUser,Motivation,Review,WishList,Profile
 
 # Register your models here.
+
+admin.site.register(StudentUser)
+admin.site.register(Profile)
+admin.site.register(Motivation)
+admin.site.register(Review)
+admin.site.register(WishList)
--- backend/student/urls.py
+++ backend/student/urls.py
@@ -1,18 +1,38 @@
-from django.urls import path,include
-from . import views
-from django.conf import settings
-from rest_framework import routers
+from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 from django.conf.urls.static import static
-from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings
 
 
-router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-
+from .views import (
+    AuthUserRegistrationView,
+    AuthUserLoginView,
+    UserListView
+)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('auth/',obtain_auth_token),
+    path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('register', AuthUserRegistrationView.as_view(), name='register'),
+    path('login', AuthUserLoginView.as_view(), name='login'),
+    path('users', UserListView.as_view(), name='users')
 ]
+
+
+# from django.urls import path,include
+# from . import views
+# from rest_framework import routers
+
+# from rest_framework.authtoken.views import obtain_auth_token
+
+
+# router = routers.DefaultRouter()
+# # router.register(r'users', views.UserViewSet)
+
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('auth/',obtain_auth_token),
+# ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
