b3dc6ef8e925325103e3ee5081161bebc5d136d5
Robert Maina <robert.maina@student.moringaschool.com>
2021-10-07 11:01:27 +0300

    Added more api

--- backend/student/urls.py
+++ backend/student/urls.py
@@ -7,24 +7,31 @@ from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 from django.conf.urls.static import static
 from django.conf import settings
+
+
 from .views import (
     AuthUserRegistrationView,
     AuthUserLoginView,
-    GetProfileView,
-    UserListView
+    UserListView,
 )
 
 urlpatterns = [
+    url(r'^motivation/$', views.MotivationList.as_view()),
+    url(r'motivation/mot-id/(?P<pk>[0-9]+)/$', views.MotivationalDescription.as_view()),
+    url(r'motivation/mot-cat/(?P<cat_pk>[0-9]+)/$', views.MotivationalByCategory.as_view()),
+
+    url(r'^review/$', views.ReviewList.as_view()),
+    url(r'review/rev-id/(?P<pk>[0-9]+)/$', views.ReviewDescription.as_view()),
+
+    url(r'^category/$', views.CategoryList.as_view()),
+    url(r'^category/cat_idd/(?P<cat_pk>[0-9]+)$', views.category_id),
+    
+    
     path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('register', AuthUserRegistrationView.as_view(), name='register'),
     path('login', AuthUserLoginView.as_view(), name='login'),
     path('users', UserListView.as_view(), name='users'),
-    path('profile/',views.profile, name='profile'),
-    url(r'^api/motivation/$', views.MotivationList.as_view()),
-    url(r'^api/review/$', views.ReviewList.as_view()),
-    url(r'api/motivation/mot-id/(?P<pk>[0-9]+)/$', views.MotivationalDescription.as_view()),
-    url(r'api/review/rev-id/(?P<pk>[0-9]+)/$', views.ReviewDescription.as_view())
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
