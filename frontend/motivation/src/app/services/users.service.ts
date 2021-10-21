8100076fefd834c3bf38695a927432087272e65b
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-10-21 14:42:10 +0300

    changes

--- frontend/motivation/src/app/services/users.service.ts
+++ frontend/motivation/src/app/services/users.service.ts
@@ -3,34 +3,25 @@ import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
-
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-
   usersUrl = environment.URL
-
    // http options used for making API calls
    private httpOptions: any;
-  
    // the actual JWT token
   public token: any;
-
   // error messages received from the login attempt
-  
-  
-
   constructor(private http:HttpClient) {
-   
    }
- 
-  getUsers(){    
+  getUsers(){
    return this.http.get(this.usersUrl + 'users')
-   
   }
-
-  
+  ChangeAdmin(id:any, data:any){
+    return this.http.put(this.usersUrl+ `superuser/${id}`, data)
+  }
+  deactivateUser(data:any){
+    return this.http.put(this.usersUrl + 'remove_user', data)
+  }
 }
-
-  
